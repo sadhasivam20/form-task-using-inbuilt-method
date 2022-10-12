fa9acacd83dc277c3f58badda3c50ffa8cf21ebf
SADHASIVAM <112924664+sadhasivam20@users.noreply.github.com>
2022-10-12 12:25:30 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,8 @@
+function foo(){
+    var a=document.getElementById("first").value;
+    var b=document.getElementById("middle").value;
+    var c=document.getElementById("last").value;
+    var d=document.getElementById("email").value;
+    var e=document.getElementById("pass").value;
+     console.log(a,b,c,d,e);
+}
